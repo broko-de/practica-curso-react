d3f6e2864dc790880c40f58de191ed14195d5984
Fede Liquin <josefliquin@gmail.com>
2025-04-13 23:06:09 -0300

    Se sube projecto de 4 linea

---
+++ 01-cuatro-en-linea/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+const TURNS = {
+  X: 'X',
+  O: 'O'
+}
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Cuatro en linea</h1>
+    </>
+  )
+}
+
+export default App
